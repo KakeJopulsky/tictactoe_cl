87239e7ebc80bcd5547d831504104cd58f26e46b
Jake Kopulsky <jakekopulsky@gmail.com>
2018-05-31 16:50:48 -0700

    Added game restart and win check

--- game.js
+++ game.js
@@ -12,16 +12,30 @@ const board = {
   9: ''
 };
 
+function newGame() {
+  for(let square in board) { board[square] = ''; };
+  console.log('Would you like to start a new game? y or n');
+  prompt.get([`answer`], (err, result) => {
+    if (result.answer === 'y') { nextMove(); }
+    if (result.answer === 'n') { process.exit() }
+    else { 
+      console.log('Invalid input!');
+      newGame();
+    }
+  });
+}
+
 function checkWin() {
   combos = [[1,2,3], [3,4,5], [6,7,8], [1,5,9], [3,5,7], [1,4,7], [2,5,8], [3,6,9]];
   for(let i = 0; i < combos.length; i += 1) {
     if (board[combos[i][0]] === 'X' && board[combos[i][1]] === 'X' && board[combos[i][2]] === 'X') {
-      console.log(' X wins!');
+      return true;
     }
     if (board[combos[i][0]] === 'O' && board[combos[i][1]] === 'O' && board[combos[i][2]] === 'O') {
-      console.log(' O wins!');
+      return true;
     }
-  } 
+  }
+  return false; 
 }
 
 function showBoard() {
@@ -41,10 +55,14 @@ function nextMove(count) {
   count = count || 0
   count % 2 === 0 ? player = 'X' : player = 'O';
   console.log(`${player}'s turn!`);
-  prompt.get([`move`], function (err, result) {
+  prompt.get([`move`], (err, result) => {
     board[result.move] = player;
     showBoard();
-    checkWin();
+    if(checkWin()) {
+      console.log(`${player} wins!`);
+      player = 'X';
+      newGame();
+    }
     nextMove(++count);
   });
 }
